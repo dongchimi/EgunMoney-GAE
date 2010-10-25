99f036ec2681af1f60469ce6ac1ff5de062c0537
dongchimi <dongchimi@2ac12d74-038a-e153-8980-8837589f15cf>
2010-10-25 11:45:53 +0000

    회원가입 / maven-gae-plugin 버전 수정

--- EGunMoneyWeb/src/main/java/com/appspot/egunmoney/domain/EgunUser.java
+++ EGunMoneyWeb/src/main/java/com/appspot/egunmoney/domain/EgunUser.java
@@ -3,6 +3,7 @@ package com.appspot.egunmoney.domain;
 import java.util.List;
 
 import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
@@ -14,7 +15,7 @@ import com.google.appengine.api.datastore.Key;
  * @author dklee
  * @since 2010.08.31
  */
-@PersistenceCapable
+@PersistenceCapable(identityType=IdentityType.DATASTORE)
 public class EgunUser{
 	
 	/** 오브젝트 id */
