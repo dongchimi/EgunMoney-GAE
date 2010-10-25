d0c11d8812a8b5dd9da8841b1dcd17d4f45174eb
dongchimi <dongchimi@2ac12d74-038a-e153-8980-8837589f15cf>
2010-10-25 11:44:45 +0000

    사용자등록 수정

--- EGunMoneyWeb/src/main/java/com/appspot/egunmoney/service/logic/EgunUserLogic.java
+++ EGunMoneyWeb/src/main/java/com/appspot/egunmoney/service/logic/EgunUserLogic.java
@@ -9,32 +9,28 @@ import org.springframework.stereotype.Component;
 import com.appspot.egunmoney.constant.EgunMoneyConstant;
 import com.appspot.egunmoney.domain.EgunUser;
 import com.appspot.egunmoney.service.EgunUserService;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 
 @Component
 public class EgunUserLogic implements EgunUserService {
 	
-	private PersistenceManagerFactory pmfInstance = JDOHelper.getPersistenceManagerFactory(EgunMoneyConstant.PERSISTENCE_MANAGER_FACTORY_NAME);
-	
-	private PersistenceManager pm = null; 
+	private PersistenceManagerFactory pmfInstance = JDOHelper.getPersistenceManagerFactory(EgunMoneyConstant.PMFN);
+	private PersistenceManager pm = null;
 	
 	@Override
 	public long registerUser(EgunUser user) {
 		try {
 			pm = pmfInstance.getPersistenceManager();
-			
-			Key key = KeyFactory.createKey(EgunUser.class.getSimpleName(), user.getUserEmail());
-			user.setOid(key);
 			pm.makePersistent(user);
 		} finally {
-			pm.close();
+			if(!pm.isClosed()) {
+				pm.close();
+			}
 		}
 		return user.getOid().getId();
 	}
 
 	@Override
-	public boolean signInEgunMoney(EgunUser user) {
+	public boolean hasEgunUser(EgunUser user) {
 		return false;
 	}
 
@@ -42,8 +38,7 @@ public class EgunUserLogic implements EgunUserService {
 	public EgunUser getUserInfo(String email) {
 		EgunUser user = null;
 		try {
-			pm = pmfInstance.getPersistenceManager();
-			user = pm.getObjectById(EgunUser.class, email);
+			user = (EgunUser) pm.getObjectById(email);
 		} finally {
 			pm.close();
 		}
