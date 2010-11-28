7e81b0d8b8bcdf2e4071684a9ec3bca7eba99a04
dongchimi <dongchimi@2ac12d74-038a-e153-8980-8837589f15cf>
2010-11-28 13:05:22 +0000

    서비스명 및 AccountBookItem 등록 로직 완료

---
+++ EGunMoneyWeb/src/main/java/com/appspot/egunmoney/service/logic/AccountBookItemLogic.java
@@ -0,0 +1,29 @@
+package com.appspot.egunmoney.service.logic;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+
+import com.appspot.egunmoney.constant.EgunMoneyConstant;
+import com.appspot.egunmoney.domain.AccountBookItem;
+import com.appspot.egunmoney.service.AccountBookItemService;
+import com.google.appengine.api.datastore.Key;
+
+public class AccountBookItemLogic implements AccountBookItemService {
+	
+	private PersistenceManagerFactory pmfInstance = JDOHelper.getPersistenceManagerFactory(EgunMoneyConstant.PMFN);
+	private PersistenceManager pm = null;
+	
+	@Override
+	public Key registerAccountBookItem(AccountBookItem item) {
+		try {
+			pm = pmfInstance.getPersistenceManager();
+			pm.makePersistent(item);
+		} finally {
+			if(!pm.isClosed()) {
+				pm.close();
+			}
+		}
+		return item.getoId();
+	}
+}
